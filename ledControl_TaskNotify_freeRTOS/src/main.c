fc3f55c62eadf101b1350aa4f00677dfca2dfc43
abdullahYuksel <ehm.abdullahyuksel@gmail.com>
2022-06-01 01:58:00 +0300

    led control with button using to xTaskNotify API in freeRTOS

---
+++ ledControl_TaskNotify_freeRTOS/src/main.c
@@ -0,0 +1,202 @@
+
+/******************************************************************************
+ ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+ ||||->file    : main.c 			|||||||||||||||||||||||||||||||||||||||||||
+ ||||->author  : Abdullah YUKSEL 	|||||||||||||||||||||||||||||||||||||||||||
+ ||||->version : v.1.0 				|||||||||||||||||||||||||||||||||||||||||||
+ ||||->date    : 01.06.2022			|||||||||||||||||||||||||||||||||||||||||||
+ ||||->brief   : main 				|||||||||||||||||||||||||||||||||||||||||||
+ ||||->update  : 01.06.2022	 		|||||||||||||||||||||||||||||||||||||||||||
+ ||||->company : YUKSEL 			|||||||||||||||||||||||||||||||||||||||||||
+ ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+*******************************************************************************/
+
+/* Includes */
+#include "stm32f4xx.h"
+#include "stm32f4_discovery.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+typedef enum
+{
+	ms_100,
+	ms_250,
+	ms_500,
+	ms_750,
+	ms_1000,
+	ms_TOTAL
+}msecTime;
+
+typedef struct
+{
+	msecTime count;
+}ledConf_t;
+
+TaskHandle_t myButtonTaskHandle = NULL;
+TaskHandle_t myLedTaskHandle = NULL;
+
+void setupConfig(void);
+void gpioConfig(void);
+
+void readButtonStateTask(void *params);
+void ledFlashTask(void *params);
+
+void delay_freeRTOS(uint32_t delayMS);
+
+ledConf_t ledConf;
+
+int main(void)
+{
+
+	RCC_DeInit();		//	hseoff | hsi on | plloff
+
+	SystemCoreClockUpdate();	// 	systemclock update to 16mhz
+
+	setupConfig();
+
+	xTaskCreate(readButtonStateTask,
+				"Buton Okuma Görevi",
+				configMINIMAL_STACK_SIZE*5,
+				NULL,
+				2,
+				&myButtonTaskHandle);
+
+	xTaskCreate(ledFlashTask,
+				"Led Kontrol Görevi",
+				configMINIMAL_STACK_SIZE*5,
+				NULL,
+				2,
+				&myLedTaskHandle);
+
+	vTaskStartScheduler();
+
+  /* Infinite loop */
+  while (1)
+  {
+
+  }
+}
+
+void readButtonStateTask(void *params)
+{
+	while(1)
+	{
+		if(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_0)	==	Bit_SET)
+		{
+//			while(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_0)	==	Bit_SET);
+			delay_freeRTOS(100);
+			xTaskNotify(myLedTaskHandle, 0x00, eIncrement);	//increment value send to led task
+		}
+	}
+}
+
+void ledFlashTask(void *params)
+{
+	uint32_t pulNotificationValue = 0;
+	while(1)
+	{
+		if(xTaskNotifyWait(0x00, 0x00, &pulNotificationValue, 100) == pdTRUE)
+		{
+			ledConf.count = pulNotificationValue % ms_TOTAL;
+		}
+			switch (ledConf.count)
+			{
+				case ms_100:
+				{
+					GPIO_ToggleBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+					vTaskDelay(100);
+					break;
+				}
+				case ms_250:
+				{
+					GPIO_ToggleBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+					vTaskDelay(250);
+					break;
+				}
+				case ms_500:
+				{
+					GPIO_ToggleBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+					vTaskDelay(500);
+					break;
+				}
+				case ms_750:
+				{
+					GPIO_ToggleBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+					vTaskDelay(750);
+					break;
+				}
+				case ms_1000:
+				{
+					GPIO_ToggleBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+					vTaskDelay(1000);
+					break;
+				}
+				default:
+				{
+					GPIO_ResetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+					break;
+				}
+			}
+
+
+	}
+}
+
+void setupConfig(void)
+{
+	gpioConfig();
+}
+void gpioConfig(void)
+{
+	//button configuration
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+	GPIO_InitTypeDef GpioInitStruct_B = {0};
+
+	GpioInitStruct_B.GPIO_Mode 	=	GPIO_Mode_IN;
+	GpioInitStruct_B.GPIO_OType 	=	GPIO_OType_PP;
+	GpioInitStruct_B.GPIO_Pin		=	GPIO_Pin_0;
+	GpioInitStruct_B.GPIO_PuPd	=	GPIO_PuPd_NOPULL;
+	GpioInitStruct_B.GPIO_Speed	=	GPIO_Speed_100MHz;
+
+	GPIO_Init(GPIOA, &GpioInitStruct_B);
+
+	//leds configuration
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+	GPIO_InitTypeDef GpioInitStruct_L = {0};
+
+	GpioInitStruct_L.GPIO_Mode 	=	GPIO_Mode_OUT;
+	GpioInitStruct_L.GPIO_OType 	=	GPIO_OType_PP;
+	GpioInitStruct_L.GPIO_Pin		=	GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+	GpioInitStruct_L.GPIO_PuPd	=	GPIO_PuPd_NOPULL;
+	GpioInitStruct_L.GPIO_Speed	=	GPIO_Speed_100MHz;
+
+	GPIO_Init(GPIOD, &GpioInitStruct_L);
+
+}
+
+void delay_freeRTOS(uint32_t delayMS)
+{
+	uint32_t currentTICK = xTaskGetTickCount();
+	uint32_t delayTICK = pdMS_TO_TICKS(delayMS); //delayMS * configTICK_RATE_HZ / 1000; // milisecond to RTOS_TICK value
+	while(xTaskGetTickCount() < (currentTICK + delayTICK));
+}
+
+/*
+ * Callback used by stm32f4_discovery_audio_codec.c.
+ * Refer to stm32f4_discovery_audio_codec.h for more info.
+ */
+void EVAL_AUDIO_TransferComplete_CallBack(uint32_t pBuffer, uint32_t Size){
+  /* TODO, implement your code here */
+  return;
+}
+
+/*
+ * Callback used by stm324xg_eval_audio_codec.c.
+ * Refer to stm324xg_eval_audio_codec.h for more info.
+ */
+uint16_t EVAL_AUDIO_GetSampleCallBack(void){
+  /* TODO, implement your code here */
+  return -1;
+}
