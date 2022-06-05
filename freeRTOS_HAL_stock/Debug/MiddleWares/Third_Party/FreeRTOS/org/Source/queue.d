653cfb73b749e216f8b28c80d73d018852dbee42
abdullahYuksel <ehm.abdullahyuksel@gmail.com>
2022-06-05 22:48:33 +0300

    freeRTOS HAL library stock example

---
+++ freeRTOS_HAL_stock/Debug/MiddleWares/Third_Party/FreeRTOS/org/Source/queue.d
@@ -0,0 +1,24 @@
+MiddleWares/Third_Party/FreeRTOS/org/Source/queue.o: \
+ ../MiddleWares/Third_Party/FreeRTOS/org/Source/queue.c \
+ D:/stmEgitim/rtos/workspace_1.9.0_freeRTOS/freeRTOS_HAL_stock/MiddleWares/Third_Party/FreeRTOS/org/Source/include/FreeRTOS.h \
+ D:/stmEgitim/rtos/workspace_1.9.0_freeRTOS/freeRTOS_HAL_stock/freeRTOS_Config/FreeRTOSConfig.h \
+ D:/stmEgitim/rtos/workspace_1.9.0_freeRTOS/freeRTOS_HAL_stock/MiddleWares/Third_Party/FreeRTOS/org/Source/include/projdefs.h \
+ D:/stmEgitim/rtos/workspace_1.9.0_freeRTOS/freeRTOS_HAL_stock/MiddleWares/Third_Party/FreeRTOS/org/Source/include/portable.h \
+ D:/stmEgitim/rtos/workspace_1.9.0_freeRTOS/freeRTOS_HAL_stock/MiddleWares/Third_Party/FreeRTOS/org/Source/include/deprecated_definitions.h \
+ D:/stmEgitim/rtos/workspace_1.9.0_freeRTOS/freeRTOS_HAL_stock/MiddleWares/Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/stmEgitim/rtos/workspace_1.9.0_freeRTOS/freeRTOS_HAL_stock/MiddleWares/Third_Party/FreeRTOS/org/Source/include/mpu_wrappers.h \
+ D:/stmEgitim/rtos/workspace_1.9.0_freeRTOS/freeRTOS_HAL_stock/MiddleWares/Third_Party/FreeRTOS/org/Source/include/task.h \
+ D:/stmEgitim/rtos/workspace_1.9.0_freeRTOS/freeRTOS_HAL_stock/MiddleWares/Third_Party/FreeRTOS/org/Source/include/list.h \
+ D:/stmEgitim/rtos/workspace_1.9.0_freeRTOS/freeRTOS_HAL_stock/MiddleWares/Third_Party/FreeRTOS/org/Source/include/queue.h \
+ D:/stmEgitim/rtos/workspace_1.9.0_freeRTOS/freeRTOS_HAL_stock/MiddleWares/Third_Party/FreeRTOS/org/Source/include/task.h
+D:/stmEgitim/rtos/workspace_1.9.0_freeRTOS/freeRTOS_HAL_stock/MiddleWares/Third_Party/FreeRTOS/org/Source/include/FreeRTOS.h:
+D:/stmEgitim/rtos/workspace_1.9.0_freeRTOS/freeRTOS_HAL_stock/freeRTOS_Config/FreeRTOSConfig.h:
+D:/stmEgitim/rtos/workspace_1.9.0_freeRTOS/freeRTOS_HAL_stock/MiddleWares/Third_Party/FreeRTOS/org/Source/include/projdefs.h:
+D:/stmEgitim/rtos/workspace_1.9.0_freeRTOS/freeRTOS_HAL_stock/MiddleWares/Third_Party/FreeRTOS/org/Source/include/portable.h:
+D:/stmEgitim/rtos/workspace_1.9.0_freeRTOS/freeRTOS_HAL_stock/MiddleWares/Third_Party/FreeRTOS/org/Source/include/deprecated_definitions.h:
+D:/stmEgitim/rtos/workspace_1.9.0_freeRTOS/freeRTOS_HAL_stock/MiddleWares/Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h:
+D:/stmEgitim/rtos/workspace_1.9.0_freeRTOS/freeRTOS_HAL_stock/MiddleWares/Third_Party/FreeRTOS/org/Source/include/mpu_wrappers.h:
+D:/stmEgitim/rtos/workspace_1.9.0_freeRTOS/freeRTOS_HAL_stock/MiddleWares/Third_Party/FreeRTOS/org/Source/include/task.h:
+D:/stmEgitim/rtos/workspace_1.9.0_freeRTOS/freeRTOS_HAL_stock/MiddleWares/Third_Party/FreeRTOS/org/Source/include/list.h:
+D:/stmEgitim/rtos/workspace_1.9.0_freeRTOS/freeRTOS_HAL_stock/MiddleWares/Third_Party/FreeRTOS/org/Source/include/queue.h:
+D:/stmEgitim/rtos/workspace_1.9.0_freeRTOS/freeRTOS_HAL_stock/MiddleWares/Third_Party/FreeRTOS/org/Source/include/task.h:
